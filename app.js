4e0dc7d32fd580791ed5cf4f68532a08bce936da
Lemnsa <legeryennyuy@gmail.com>
2024-08-01 00:39:47 +0100

    Get Number Of Squares From User

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const divContainers = document.getElementById("container");
 
-let appendDiv = (container) => {
+const numberOfDivsByUser = Number(prompt("Enter the number Of Squares"));
+
+if (numberOfDivsByUser > 100) {
+    alert("Enter a Number not greater than 100!");
+}
+else{
+    const numberOfRowsAndColumns = Math.sqrt(numberOfDivsByUser);
+    const rounded = Math.ceil(numberOfRowsAndColumns);
+
+    console.log(rounded);
+    let appendDiv = (container) => {
     const div = document.createElement("div");
-    div.setAttribute("style", "flex: 1 0 calc(100% / 16); height: calc(100% / 16");
+    div.setAttribute("style", `flex: 1 0 calc(100% / ${rounded}); height: calc(100% / ${rounded}`);
     return container.appendChild(div);
 }
 
-for(let i = 0; i < 256; i++) {
+    for(let i = 0; i < numberOfDivsByUser; i++) {
 
         appendDiv(divContainers);
+    }
 }
 
 function getRandomColor() {
