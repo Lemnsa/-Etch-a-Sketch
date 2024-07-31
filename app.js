666321e7c6491be21c4fb3d1e5ecb5b525b903c0
Lemnsa <legeryennyuy@gmail.com>
2024-07-31 22:26:48 +0100

    Move CSS Rows and Columns to Javascript

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const divContainers = document.getElementById("container");
 
 let appendDiv = (container) => {
     const div = document.createElement("div");
+    div.setAttribute("style", "flex: 1 0 calc(100% / 16); height: calc(100% / 16");
     return container.appendChild(div);
 }
 
